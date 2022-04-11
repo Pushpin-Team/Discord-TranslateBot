795785186503617bec107ffad8e93da3a059a66b
SHUSTRIK-Milan <shustrikofficial.milan@gmail.com>
2022-04-11 15:43:29 +0500

    fix intraction.error

--- _Functions/Interaction/error.js
+++ _Functions/Interaction/error.js
@@ -4,31 +4,5 @@ module.exports = (interaction, error, ephemeral) => {
     console.log(error);
     if(!error.message) error.message = '';
 
-    CMD.Interaction.reply(interaction, {content: `> Ошибка. ${error.message} ⛔`, embeds: [], components: [], ephemeral: ephemeral});
-    guild.channels.cache.get(Config.channelsID.errors).send({
-        embeds: [
-            {
-                title: `Ошибка "${error.message}"`,
-                description: `${error.stack}`,
-                timestamp: new Date(),
-                color: "#DD2C2C",
-                thumbnail: {url: 'https://i.imgur.com/YTXH8fJ.png'}
-            }
-        ],
-        components: [
-            {
-                type: 'ACTION_ROW',
-                components: [
-                    {
-                        type: 'BUTTON',
-                        label: 'Канал',
-                        style: 'LINK',
-                        url: `discord:///channels/${interaction.guildId}/${interaction.channelId}`
-                    }
-                ]
-            }
-        ]
-    });
-
     return error;
 }
