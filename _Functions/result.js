5afed5dac7044e4e70410cce7b54cc9b93813e04
SHUSTRIK-Milan <shustrikofficial.milan@gmail.com>
2022-04-25 13:21:18 +0500

    new error message

--- _Functions/result.js
+++ _Functions/result.js
@@ -16,70 +16,52 @@ module.exports = async (interaction) => { const CMD = require('../bot.js'); try 
     const language = interaction.customId.split('_')[1];
     CMD.translateCache.delete(interaction.message.interaction.id);
 
-    var translate;
-    try {
-        translate = await CMD.Translate(text, {to: language});
-    } catch(error) {
-        console.log(error);
-        return;
-    }
+    var translate = await CMD.Translate(text, {to: language});
     const from_info = CMD.buttons.getInfo(translate.from.language.iso);
     const to_info = CMD.buttons.getInfo(language);
 
     console.log(translate.from.language.iso)
     console.log(language)
 
-    if(!from_info || !to_info) {
-        CMD.Interaction.reply(interaction, {
-            embeds: [
-                {
-                    description: '❌ **Failed to detect language**',
-                    color: "#528ff5", 
-                }
-            ],
-            components: []
-        });
-    } else {
-        CMD.Interaction.reply(interaction, {
-            embeds: [
-                {
-                    description: translate ? `${translate.text}` : '❌ **Error**',
-                    color: "#528ff5",
-                }
-            ],
-            components: [
-                {
-                    type: 'ACTION_ROW',
-                    components: [
-                        {
-                            type: 'BUTTON',
-                            label: `${from_info.name} [${from_info.code}]`,
-                            customId: 'null_from',
-                            style: 'PRIMARY',
-                            emoji: from_info.emoji,
-                            disabled: true
-                        },
-                        {
-                            type: 'BUTTON',
-                            label: ` `,
-                            customId: 'null_line',
-                            style: 'PRIMARY',
-                            emoji: '➡',
-                            disabled: true
-                        },
-                        {
-                            type: 'BUTTON',
-                            label: `${to_info.name} [${to_info.code}]`,
-                            customId: 'null_to',
-                            style: 'PRIMARY',
-                            emoji: to_info.emoji,
-                            disabled: true
-                        },
-                    ]
-                }
-            ]
-        });
-    }   
+    CMD.Interaction.reply(interaction, {
+        embeds: [
+            {
+                description: translate.text,
+                color: "#528ff5",
+            }
+        ],
+        components: [
+            {
+                type: 'ACTION_ROW',
+                components: [
+                    {
+                        type: 'BUTTON',
+                        label: `${from_info.name} [${from_info.code}]`,
+                        customId: 'null_from',
+                        style: 'PRIMARY',
+                        emoji: from_info.emoji,
+                        disabled: true
+                    },
+                    {
+                        type: 'BUTTON',
+                        label: ` `,
+                        customId: 'null_line',
+                        style: 'PRIMARY',
+                        emoji: '➡',
+                        disabled: true
+                    },
+                    {
+                        type: 'BUTTON',
+                        label: `${to_info.name} [${to_info.code}]`,
+                        customId: 'null_to',
+                        style: 'PRIMARY',
+                        emoji: to_info.emoji,
+                        disabled: true
+                    },
+                ]
+            }
+        ]
+    });   
 } catch (error) {
-    CMD.Interaction.error(interaction, error, true);
+    CMD.Interaction.error(interaction, `❌ **Error in translation**\n> ${error}`, true);
 }}
